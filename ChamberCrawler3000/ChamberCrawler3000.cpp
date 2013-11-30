ee25a548754195576dc70410a941174c610e72e1
Lucille Lu <driftlulu@gmail.com>
2013-11-30 17:10:14 -0500

    finished move functions
    move function now works

--- ChamberCrawler3000/ChamberCrawler3000.cpp
+++ ChamberCrawler3000/ChamberCrawler3000.cpp
@@ -5,15 +5,15 @@
 #include "stdafx.h"
 #include "floor.h"
 
-char PlayerASCII = 0x01;    // ASCII Man
+char PlayerSymbol = 0x01;    // ASCII Man
 char DuckASCII = 0x44;    // ASCII Duck
-char VerticalWallASCII = 0xBA;    // Vertical wall piece
-char HorizontalWallASCII = 0xCD;    // Horizontal wall piece
-char TopRightCornerASCII = 0xBB;    // Top right wall piece
-char TopLeftCornerASCII = 0xC9;    // Top left wall piece
-char BotLeftCornerASCII = 0xC8;    // Bottom right wall piece
-char BotRightCornerASCII = 0xBC;    // Bottom left wall piece
-char FloorTileASCII = 0x2E;    // Floor tile 
+char VerticalWallSymbol = 0xBA;    // Vertical wall piece
+char HorizontalWallSymbol = 0xCD;    // Horizontal wall piece
+char TopRightCornerSymbol = 0xBB;    // Top right wall piece
+char TopLeftCornerSymbol = 0xC9;    // Top left wall piece
+char BotLeftCornerSymbol = 0xC8;    // Bottom right wall piece
+char BotRightCornerSymbol = 0xBC;    // Bottom left wall piece
+char FloorTileSymbol = 0x2E;    // Floor tile 
 
 
 using namespace std;
